d0a5f1dc60e2442b191ba04382c5d3f9d14b43ce
kz <rkdevmail@gmail.com>
2017-03-04 14:06:49 -0600

    basic todo template rendering

--- src/main.js
+++ src/main.js
@@ -1,45 +1,14 @@
 /*jshint esversion:6*/     //tells linter to be quiet about node syntax
-import Vue from 'vue';
 require('../index.html');
 //require('../index.css');
 
-Vue.component('my-component', {
-  template: `
-  <section>
-    <header>
-      <h1>todos<h1>
-        <input>
-        <br/>
-    </header>
-    <section>
-      <input type="checkbox">
-      <ul>
-        <li>
-          <div>
-          <input type="checkbox">
-          <label>Label</label>
-          <button>X</button>
-          </div>
-          <input>
-        </li>
-      </ul>
-    </section>
-    <footer>
-      <span>Count</span>
-      <nav>
-        <ul>
-        <li><a href="#all">All</a></li>
-        <li><a href="#active">Active</a></li>
-        <li><a href="#completed">Completed</a></li>
-        </ul>
-      </nav>
-      <button>Clear Completed</button>
-    </footer>
-  </section>
-`
+import Vue from 'vue';
+import todo from './components/todo.vue';
 
-});
-
-new Vue({
-  el: '#example'
+window.myVue = new Vue({
+  el: '#app',
+//  render: callback => callback(todo)
+  render: function(callback){
+    return callback(todo);
+  }
 });
